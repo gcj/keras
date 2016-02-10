f861ae69de7597b37e71e06fd6bf11b1c73495ee
Chenjie Gu <chenjie.gu@gmail.com>
2016-02-09 22:23:40 -0800

    Added TripletMerge layer.

--- keras/layers/MyLayers.py
+++ keras/layers/MyLayers.py
@@ -5,12 +5,13 @@ import numpy as np
 from .. import backend as K
 from .. import activations, initializations
 from ..layers.core import Layer, MaskedLayer
+from ..activations import relu, sigmoid
 
 class PairMerge(Layer):
     """
     merge a pair of inputs
     """
-    def __init__(self, input1, input2, mode='sum', concat_axis=-1, dot_axes=-1):
+    def __init__(self, input1, input2, include_previous_layers=False, mode='sum', concat_axis=-1, dot_axes=-1):
         if mode not in {'sum', 'mul', 'concat', 'ave', 'join', 'cos', 'dot',
                         'l2', 'l1', 'hamming'}:
             raise Exception('Invalid merge mode: ' + str(mode))
@@ -23,15 +24,20 @@ class PairMerge(Layer):
         self.regularizers = []
         self.constraints = []
         self.updates = []
-        for l in self.layers:
-            params, regs, consts, updates = l.get_params()
-            self.regularizers += regs
-            self.updates += updates
-            # params and constraints have the same size
-            for p, c in zip(params, consts):
-                if p not in self.trainable_weights:
-                    self.trainable_weights.append(p)
-                    self.constraints.append(c)
+        if include_previous_layers == True:
+            #print 'Yeah'
+            for l in self.layers:
+                params, regs, consts, updates = l.get_params()
+                #print params
+                self.regularizers += regs
+                self.updates += updates
+                # params and constraints have the same size
+                for p, c in zip(params, consts):
+                    if p not in self.trainable_weights:
+                        self.trainable_weights.append(p)
+                        self.constraints.append(c)
+                #print self.trainable_weights
+            #print 'OOH'
         super(PairMerge, self).__init__()
         
     @property
@@ -57,6 +63,110 @@ class PairMerge(Layer):
             return s1 * s2
         elif self.mode == 'l2':
             return K.sum(K.square(s1-s2), axis=-1, keepdims=True)
+        elif self.mode == 'l1':
+            return K.sum(K.abs(s1-s2))
+        else:
+            raise Exception('Unknown merge mode.')
+
+    def get_input(self, train=False):
+        res = []
+        for i in range(len(self.layers)):
+            o = self.layers[i].get_input(train)
+            if not type(o) == list:
+                o = [o]
+            for output in o:
+                if output not in res:
+                    res.append(output)
+        return res
+
+    @property
+    def input(self):
+        return self.get_input()
+    
+    def supports_masked_input(self):
+        return False
+
+    def get_output_mask(self, train=None):
+        return None
+
+    def get_weights(self):
+        weights = []
+        for l in self.layers:
+            weights += l.get_weights()
+        return weights
+
+    def set_weights(self, weights):
+        for i in range(len(self.layers)):
+            nb_param = len(self.layers[i].trainable_weights)
+            self.layers[i].set_weights(weights[:nb_param])
+            weights = weights[nb_param:]
+
+    def get_config(self):
+        config = {'name': self.__class__.__name__,
+                  'layers': [l.get_config() for l in self.layers],
+                  'mode': self.mode,
+                  'concat_axis': self.concat_axis,
+                  'dot_axes': self.dot_axes}
+        base_config = super(PairMerge, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+class TripletMerge(Layer):
+    """
+    merge a pair of inputs
+    """
+    def __init__(self, input1, input2, input3, include_previous_layers=False, mode='sum', concat_axis=-1, dot_axes=-1):
+        if mode not in {'sum', 'mul', 'concat', 'ave', 'join', 'cos', 'dot',
+                        'l2', 'l1', 'hamming', 'l2diff_relu'}:
+            raise Exception('Invalid merge mode: ' + str(mode))
+        
+        self.mode = mode
+        self.concat_axis = concat_axis
+        self.dot_axes = dot_axes
+        self.layers = [input1, input2, input3]
+        self.trainable_weights = []
+        self.regularizers = []
+        self.constraints = []
+        self.updates = []
+        if include_previous_layers == True:
+            #print 'Yeah'
+            for l in self.layers:
+                params, regs, consts, updates = l.get_params()
+                #print params
+                self.regularizers += regs
+                self.updates += updates
+                # params and constraints have the same size
+                for p, c in zip(params, consts):
+                    if p not in self.trainable_weights:
+                        self.trainable_weights.append(p)
+                        self.constraints.append(c)
+                #print self.trainable_weights
+            #print 'OOH'
+        super(TripletMerge, self).__init__()
+        
+    @property
+    def output_shape(self):
+        input_shapes = [layer.output_shape for layer in self.layers]
+        input_shape = input_shapes[0]
+        if self.mode in ['sum', 'mul', 'ave']:
+            return input_shape
+        elif self.mode in ['l2', 'l1']:
+            return (input_shape[0],1)
+        
+    def get_params(self):
+        return self.trainable_weights, self.regularizers, self.constraints, self.updates
+    
+    def get_output(self, train=False):
+        s1 = self.layers[0].get_output(train)
+        s2 = self.layers[1].get_output(train)
+        s3 = self.layers[2].get_output(train)
+        if self.mode == 'sum':
+            return s1 + s2 + s3
+        elif self.mode == 'ave':
+            return (s1 + s2 + s3)/3
+        elif self.mode == 'mul':
+            return s1 * s2 * s3
+        elif self.mode == 'l2diff_relu':
+            return sigmoid(K.sum(K.square(s1-s2), axis=-1, keepdims=True) - K.sum(K.square(s1-s3), axis=-1, keepdims=True)) 
         elif self.mode == 'l1':
             return K.sum(K.abs(s1-s2))
         else:
